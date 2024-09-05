50ac68a8674c55fcf97fee8ed88eaa55d064ac47
Mahbub Al Hasan Akib <akibh0606@gmail.com>
2024-09-05 16:24:14 +0600

    python_final_exam.py

--- python_final_exam.py
+++ python_final_exam.py
@@ -1,150 +1,168 @@
-"""
-ami ekta file e sob korsi karon vinno vinno file create kore korle amar kache jamela lage . kon file a kon function seta vule jai . 
-"""
+from abc import ABC, abstractmethod
 
-import random
+class Account(ABC):
+    accounts = []
 
-class Bank:
-    def __init__(self):
-        self.accounts = {} 
-        self.total_balance = 0
-        self.total_loan = 0
-        self.loan_feature = True
-
-    def create_account(self, name, email, address, account_type):
-        account_number = random.randint(100000, 999999)
-        for account_number in self.accounts:
-            account_number = str(random.randint(100000, 999999))
-
-        user = User(name, email, address, account_type, account_number)
-        self.accounts[account_number] = user
-        print(f"Account created successfully for {name} Account Number: {account_number}")
-        return account_number
-
-    def delete_account(self, account_number):
-        if account_number in self.accounts:
-            del self.accounts[account_number]
-            print("Account deleted successfully")
-        else:
-            print("Account does not exist")
-
-    def get_total_balance(self):
-        self.total_balance = sum(user.balance for user in self.accounts.values())
-        print(f"Total available balance in the bank: {self.total_balance}")
-
-    def get_total_loan(self):
-        print(f"Total loan amount given by the bank: {self.total_loan}")
-
-    def toggle_loan_feature(self, status):
-        self.loan_feature = status
-        state = "on" if status else "off"
-        print(f"Loan feature turned {state}.")
-
-
-class User:
-    def __init__(self, name, email, address, account_type, account_number):
+    def __init__(self, name, account_number, password, account_type):
         self.name = name
-        self.email = email
-        self.address = address
-        self.account_type = account_type
-        self.account_number = account_number
+        self.account_no = account_number
+        self.password = password
         self.balance = 0
-        self.transaction_history = []
-        self.loans_taken = 0
+        self.account_type = account_type
+        Account.accounts.append(self)
+
+    def change_info(self, name, password=None):
+        self.name = name
+        if password:
+            self.password = password
+            print(f"\n--> Name and Password are changed for Account No: {self.account_no}")
+        else:
+            print(f"\n--> Name is changed for Account No: {self.account_no}")
 
     def deposit(self, amount):
-        if amount > 0:
+        if amount >= 0:
             self.balance += amount
-            self.transaction_history.append(f"Deposited: {amount}")
-            print(f"Deposited {amount}. Current Balance: {self.balance}")
+            print(f"\n--> Deposited ${amount}. New balance: ${self.balance}")
         else:
-            print("Deposit amount must be greater than 0.")
+            print("\n--> Invalid deposit amount")
 
     def withdraw(self, amount):
-        if amount > self.balance:
-            print("Withdrawal amount exceeded.")
-        elif amount > 0:
+        if amount >= 0 and amount <= self.balance:
             self.balance -= amount
-            self.transaction_history.append(f"Withdrew: {amount}")
-            print(f"Withdrew {amount}. Current Balance: {self.balance}")
+            print(f"\n--> Withdrew ${amount}. New balance: ${self.balance}")
         else:
-            print("Withdrawal amount must be greater than 0.")
+            print("\n--> Invalid withdrawal amount")
 
-    def check_balance(self):
-        print(f"Available Balance: {self.balance}")
+    @abstractmethod
+    def show_info(self):
+        pass
 
-    def view_transaction_history(self):
-        print("Transaction History:")
-        for transaction in self.transaction_history:
-            print(transaction)
 
-    def take_loan(self, amount):
-        if self.loans_taken >= 2:
-            print("Loan limit reached.")
-        elif bank.loan_feature:
-            self.balance += amount
-            self.loans_taken += 1
-            bank.total_loan += amount
-            self.transaction_history.append(f"Took a loan: {amount}")
-            print(f"Loan of {amount} granted. Current Balance: {self.balance}")
-        else:
-            print("Loan feature is currently off.")
+class SavingsAccount(Account):
+    def __init__(self, name, account_number, password, interest_rate):
+        super().__init__(name, account_number, password, "savings")
+        self.interest_rate = interest_rate
 
-    def transfer(self, target_account_number, amount):
-        if amount > self.balance:
-            print("Insufficient balance to transfer.")
-        elif target_account_number not in bank.accounts:
-            print("Account does not exist.")
-        else:
+    def apply_interest(self):
+        interest = self.balance * (self.interest_rate / 100)
+        print("\n--> Interest is applied!")
+        self.deposit(interest)
+
+    def show_info(self):
+        print(f"Details of {self.account_type} account for {self.name}:")
+        print(f"\tAccount Type: {self.account_type}")
+        print(f"\tName: {self.name}")
+        print(f"\tAccount No: {self.account_no}")
+        print(f"\tCurrent Balance: ${self.balance}\n")
+
+
+class SpecialAccount(Account):
+    def __init__(self, name, account_number, password, limit):
+        super().__init__(name, account_number, password, "special")
+        self.limit = limit
+
+    def withdraw(self, amount):
+        if amount > 0 and (self.balance - amount) >= -self.limit:
             self.balance -= amount
-            bank.accounts[target_account_number].balance += amount
-            self.transaction_history.append(f"Transferred {amount} to {target_account_number}")
-            bank.accounts[target_account_number].transaction_history.append(f"Received {amount} from {self.account_number}")
-            print(f"Transferred {amount} to {target_account_number}. Current Balance: {self.balance}")
+            print(f"\n--> Withdrew ${amount}. New balance: ${self.balance}")
+        else:
+            print("\n--> Invalid withdrawal amount or overdraft limit reached")
+
+    def show_info(self):
+        print(f"Details of {self.account_type} account for {self.name}:")
+        print(f"\tAccount Type: {self.account_type}")
+        print(f"\tName: {self.name}")
+        print(f"\tAccount No: {self.account_no}")
+        print(f"\tCurrent Balance: ${self.balance}\n")
 
 
-class Admin:
-    def __init__(self, bank):
-        self.bank = bank
+current_user = None
 
-    def create_account(self, name, email, address, account_type):
-        return self.bank.create_account(name, email, address, account_type)
+while True:
+    if current_user is None:
+        print("\n--> No user logged in!")
+        choice = input("\n--> Register/Login (R/L): ").strip().upper()
 
-    def delete_account(self, account_number):
-        self.bank.delete_account(account_number)
+        if choice == "R":
+            name = input("Name: ")
+            account_number = input("Account Number: ")
+            password = input("Password: ")
+            account_type = input("Savings Account or Special Account (sv/sp): ").strip().lower()
 
-    def view_all_accounts(self):
-        print("All User Accounts:")
-        for account_number, user in self.bank.accounts.items():
-            print(f"Account Number: {account_number}, Name: {user.name}, Balance: {user.balance}")
+            if account_type == "sv":
+                interest_rate = float(input("Interest rate: "))
+                current_user = SavingsAccount(name, account_number, password, interest_rate)
+            elif account_type == "sp":
+                limit = float(input("Overdraft Limit: "))
+                current_user = SpecialAccount(name, account_number, password, limit)
+            else:
+                print("Invalid account type selected.")
+        elif choice == "L":
+            account_number = input("Account Number: ")
+            found = False
+            for account in Account.accounts:
+                if account.account_no == account_number:
+                    current_user = account
+                    found = True
+                    break
+            if not found:
+                print("\n--> Account not found!")
+        else:
+            print("\n--> Invalid choice!")
+    
+    else:
+        print(f"\nWelcome, {current_user.name}!\n")
+        if current_user.account_type == "savings":
+            print("1.Withdraw")
+            print("2.Deposit")
+            print("3.Show Info")
+            print("4.Change Info")
+            print("5.Apply Interest")
+            print("6.Logout\n")
 
-    def check_total_balance(self):
-        self.bank.get_total_balance()
+            option = input("Choose an option: ").strip()
 
-    def check_total_loan(self):
-        self.bank.get_total_loan()
+            if option == "1":
+                amount = float(input("Enter withdrawal amount: "))
+                current_user.withdraw(amount)
+            elif option == "2":
+                amount = float(input("Enter deposit amount: "))
+                current_user.deposit(amount)
+            elif option == "3":
+                current_user.show_info()
+            elif option == "4":
+                new_name = input("Enter new name: ")
+                new_password = input("Enter new password (leave blank to keep current): ")
+                current_user.change_info(new_name, new_password or None)
+            elif option == "5":
+                current_user.apply_interest()
+            elif option == "6":
+                current_user = None
+            else:
+                print("\n--> Invalid option!")
+        
+        elif current_user.account_type == "special":
+            print("1. Withdraw")
+            print("2. Deposit")
+            print("3. Show Info")
+            print("4. Change Info")
+            print("5. Logout\n")
 
-    def toggle_loan_feature(self, status):
-        self.bank.toggle_loan_feature(status)
+            option = int(input("Chhose Option:"))
 
-bank = Bank()
-admin = Admin(bank)
-
-account1 = admin.create_account("Akib", "akib@hasan.com", "Lakshmipur", "Savings")
-account2 = admin.create_account("Sadia", "sadia123@gmail.com", "Dhaka uttara", "Current")
-
-# User 
-user1 = bank.accounts[account1]
-user2 = bank.accounts[account2]
-
-user1.deposit(500)
-user1.withdraw(200)
-user1.check_balance()
-user1.view_transaction_history()
-
-user1.take_loan(300)
-user1.transfer(account2, 100)
-
-admin.view_all_accounts()
-admin.check_total_balance()
-admin.check_total_loan()
+            if option == "1":
+                amount = float(input("Enter withdrawal amount: "))
+                current_user.withdraw(amount)
+            elif option == "2":
+                amount = float(input("Enter deposit amount: "))
+                current_user.deposit(amount)
+            elif option == "3":
+                current_user.show_info()
+            elif option == "4":
+                new_name = input("Enter new name: ")
+                new_password = input("Enter new password: ")
+                current_user.change_info(new_name, new_password or None)
+            elif option == "5":
+                current_user = None
+            else:
+                print("\n--> Invalid option!")
